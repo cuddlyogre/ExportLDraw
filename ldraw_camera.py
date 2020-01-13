9358ab7d419cea4142e73a7faec8d335e395085f
Matthew Morrison <mmorrison@mcommgroup.com>
2020-01-13 08:41:21 -0600

    adjust camera to more closely match ldcad output

--- ldraw_camera.py
+++ ldraw_camera.py
@@ -38,18 +38,15 @@ class LDrawCamera:
         obj.hide_viewport = self.hidden
         obj.hide_render = self.hidden
 
-        if False:
-            camera.sensor_fit = 'VERTICAL'
-            camera.sensor_height = self.fov
-        else:
-            camera.lens_unit = 'FOV'
-            camera.angle = math.radians(self.fov)  # self.fov * 3.1415926 / 180.0
-
-        camera.clip_start = self.z_near * options.scale
-        camera.clip_end = self.z_far * options.scale
+        camera.sensor_fit = 'VERTICAL'
+        # camera.sensor_height = self.fov
+        camera.lens_unit = 'FOV'
+        camera.angle = math.radians(self.fov)  # self.fov * 3.1415926 / 180.0
+        camera.clip_start = self.z_near
+        camera.clip_end = self.z_far
 
         if self.orthographic:
-            dist_target_to_camera = (location - target_location).length
+            dist_target_to_camera = (self.position - self.target_position).length
             camera.ortho_scale = dist_target_to_camera / 1.92
             camera.type = 'ORTHO'
         else:
@@ -61,21 +58,36 @@ class LDrawCamera:
         if obj.name not in collection:
             collection.objects.link(obj)
 
-        obj.matrix_parent_inverse = obj.parent.matrix_world.inverted()
-
         LDrawCamera.look_at(obj, self.target_position, self.up_vector)
 
         return obj
 
     @staticmethod
     def look_at(obj, target_location, up_vector):
+        obj.matrix_parent_inverse = obj.parent.matrix_world.inverted()
         bpy.context.view_layer.update()
 
-        loc_camera = obj.matrix_world.to_translation()
+        location = obj.matrix_world.to_translation()
+
+        location.x = location.x * options.scale
+        location.y = location.y * options.scale
+        location.z = location.z * options.scale
+
+        target_location.x = target_location.x * options.scale
+        target_location.y = target_location.y * options.scale
+        target_location.z = target_location.z * options.scale
+
+        up_vector.x = up_vector.x * options.scale
+        up_vector.y = up_vector.y * options.scale
+        up_vector.z = up_vector.z * options.scale
 
         # back vector is a vector pointing from the target to the camera
-        back = loc_camera - target_location
+        back = location - target_location
         back.normalize()
+
+        obj.data.clip_start = obj.data.clip_start * options.scale
+        obj.data.clip_end = obj.data.clip_end * options.scale
+        obj.data.clip_end = obj.data.clip_end * options.scale
 
         # If our back and up vectors are very close to pointing the same way (or opposite), choose a different up_vector
         if abs(back.dot(up_vector)) > 0.9999:
@@ -89,9 +101,9 @@ class LDrawCamera:
         up = back.cross(right)
         up.normalize()
 
-        row1 = [right.x, up.x, back.x, loc_camera.x * options.scale]
-        row2 = [right.y, up.y, back.y, loc_camera.y * options.scale]
-        row3 = [right.z, up.z, back.z, loc_camera.z * options.scale]
+        row1 = [right.x, up.x, back.x, location.x]
+        row2 = [right.y, up.y, back.y, location.y]
+        row3 = [right.z, up.z, back.z, location.z]
         row4 = [0.0, 0.0, 0.0, 1.0]
 
         obj.matrix_world = mathutils.Matrix((row1, row2, row3, row4))
