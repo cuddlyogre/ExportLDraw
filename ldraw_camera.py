dcaadc4a00de25542d7e7dc97ea1b57e7709635c
Matthew Morrison <mmorrison@mcommgroup.com>
2020-01-15 22:21:10 -0600

    camera cleanup

--- ldraw_camera.py
+++ ldraw_camera.py
@@ -3,6 +3,7 @@ import mathutils
 import math
 
 from . import options
+from . import matrices
 
 
 class LDrawCamera:
@@ -30,33 +31,56 @@ class LDrawCamera:
         return LDrawCamera.cameras
 
     def create_camera_node(self, empty=None, collection=None):
-        camera = bpy.data.cameras.new(self.name)
-        obj = bpy.data.objects.new(self.name, camera)
+        obj = bpy.data.objects.new(self.name, bpy.data.cameras.new(self.name))
 
         obj.name = self.name
         obj.location = self.position
         obj.hide_viewport = self.hidden
         obj.hide_render = self.hidden
 
-        camera.sensor_fit = 'VERTICAL'
+        obj.data.sensor_fit = 'VERTICAL'
         # camera.sensor_height = self.fov
-        camera.lens_unit = 'FOV'
-        camera.angle = math.radians(self.fov)  # self.fov * 3.1415926 / 180.0
-        camera.clip_start = self.z_near
-        camera.clip_end = self.z_far
+        obj.data.lens_unit = 'FOV'
+        obj.data.angle = math.radians(self.fov)  # self.fov * 3.1415926 / 180.0
+        obj.data.clip_start = self.z_near
+        obj.data.clip_end = self.z_far
 
         if self.orthographic:
             dist_target_to_camera = (self.position - self.target_position).length
-            camera.ortho_scale = dist_target_to_camera / 1.92
-            camera.type = 'ORTHO'
+            obj.data.ortho_scale = dist_target_to_camera / 1.92
+            obj.data.type = 'ORTHO'
         else:
-            camera.type = 'PERSP'
+            obj.data.type = 'PERSP'
 
-        obj.parent = empty
+        # https://blender.stackexchange.com/a/72899
+        # https://blender.stackexchange.com/a/154926
+        # https://blender.stackexchange.com/a/29148
+        # when parenting the location of the parented obj is affected by the transform of the empty
+        # this undoes the transform of the empty
+        # obj.parent = empty
         if collection is None:
             collection = bpy.context.scene.collection
         if obj.name not in collection:
             collection.objects.link(obj)
+        obj.matrix_parent_inverse = obj.parent.matrix_world.inverted()
+        obj.matrix_world = matrices.rotation @ obj.matrix_world
+        bpy.context.view_layer.update()
+        # ===
+
+        obj.location.x = obj.location.x * options.scale
+        obj.location.y = obj.location.y * options.scale
+        obj.location.z = obj.location.z * options.scale
+
+        obj.data.clip_start = obj.data.clip_start * options.scale
+        obj.data.clip_end = obj.data.clip_end * options.scale
+
+        self.target_position.x = self.target_position.x * options.scale
+        self.target_position.y = self.target_position.y * options.scale
+        self.target_position.z = self.target_position.z * options.scale
+
+        self.up_vector.x = self.up_vector.x * options.scale
+        self.up_vector.y = self.up_vector.y * options.scale
+        self.up_vector.z = self.up_vector.z * options.scale
 
         LDrawCamera.look_at(obj, self.target_position, self.up_vector)
 
@@ -64,30 +88,9 @@ class LDrawCamera:
 
     @staticmethod
     def look_at(obj, target_location, up_vector):
-        obj.matrix_parent_inverse = obj.parent.matrix_world.inverted()
-        bpy.context.view_layer.update()
-
-        location = obj.matrix_world.to_translation()
-
-        location.x = location.x * options.scale
-        location.y = location.y * options.scale
-        location.z = location.z * options.scale
-
-        target_location.x = target_location.x * options.scale
-        target_location.y = target_location.y * options.scale
-        target_location.z = target_location.z * options.scale
-
-        up_vector.x = up_vector.x * options.scale
-        up_vector.y = up_vector.y * options.scale
-        up_vector.z = up_vector.z * options.scale
-
         # back vector is a vector pointing from the target to the camera
-        back = location - target_location
+        back = obj.location - target_location
         back.normalize()
-
-        obj.data.clip_start = obj.data.clip_start * options.scale
-        obj.data.clip_end = obj.data.clip_end * options.scale
-        obj.data.clip_end = obj.data.clip_end * options.scale
 
         # If our back and up vectors are very close to pointing the same way (or opposite), choose a different up_vector
         if abs(back.dot(up_vector)) > 0.9999:
@@ -101,9 +104,9 @@ class LDrawCamera:
         up = back.cross(right)
         up.normalize()
 
-        row1 = [right.x, up.x, back.x, location.x]
-        row2 = [right.y, up.y, back.y, location.y]
-        row3 = [right.z, up.z, back.z, location.z]
+        row1 = [right.x, up.x, back.x, obj.location.x]
+        row2 = [right.y, up.y, back.y, obj.location.y]
+        row3 = [right.z, up.z, back.z, obj.location.z]
         row4 = [0.0, 0.0, 0.0, 1.0]
 
         obj.matrix_world = mathutils.Matrix((row1, row2, row3, row4))
