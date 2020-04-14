558408bb6a98a4849185801317785785d0ad615d
Matthew Morrison <mmorrison@mcommgroup.com>
2020-04-14 14:07:33 -0500

    downloader.py

--- downloader.py
+++ downloader.py
@@ -4,6 +4,9 @@ import urllib.error
 from pathlib import Path
 
 
+# from . import filesystem
+
+
 def download_file(url, destination, headers=None):
     if headers is None:
         headers = {}
@@ -18,11 +21,14 @@ def download_file(url, destination, headers=None):
         with open(destination, 'wb') as file:
             file.write(response)
     except urllib.error.HTTPError as e:
+        print(str(e.__class__))
         print(e.code)
         print(e.reason)
     except urllib.error.ContentTooShortError as e:
+        print(str(e.__class__))
         print(str(e))
     except urllib.error.URLError as e:
+        print(str(e.__class__))
         print(e.reason)
 
 
@@ -33,15 +39,20 @@ def download_file(url, destination, headers=None):
 # filename = os.path.basename(url)
 
 def download_ldraw(path, filename):
+    filename = os.path.basename(filename)
+
     ldraw_org_url = "https://www.ldraw.org/library"
     texture_path = os.path.join(path, filename)
-    texture_url = os.path.join(ldraw_org_url, texture_path)
+    texture_url = f"{ldraw_org_url}/{path}/{filename}"
 
+    # ldraw_path = filesystem.locate_ldraw()
     home = str(Path.home())
     ldraw_path = os.path.join(home, "ldraw")
-    destination = os.path.join(ldraw_path, texture_path)
+    realpath = os.path.realpath(os.path.join(ldraw_path, texture_path))
+    destination = os.path.join(ldraw_path, texture_path, os.path.basename(realpath))
 
     headers = {'User-Agent': "Blender LDraw Importer"}
+
     download_file(texture_url, destination, headers)
 
 
