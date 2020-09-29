f54774ea5e89105a0d085f02d276ea7ff0b5eb45
Matthew Morrison <cuddlyogre@gmail.com>
2020-09-28 22:33:32 -0500

    Integrate logic from https://github.com/TobyLobster/ImportLDraw/pull/65/files

--- ldraw_geometry.py
+++ ldraw_geometry.py
@@ -31,11 +31,15 @@ class LDrawGeometry:
             vertices.append(vertex)
 
         # https://wiki.ldraw.org/wiki/LDraw_Files_Requirements#Complex_quadrilaterals
+        # https://github.com/TobyLobster/ImportLDraw/pull/65/files#diff-f5a55f4be537f9ace2c9534f1e49c82e
         if vert_count == 4:
             vA = (vertices[1] - vertices[0]).cross(vertices[2] - vertices[0])
             vB = (vertices[2] - vertices[1]).cross(vertices[3] - vertices[1])
+            vC = (vertices[3] - vertices[2]).cross(vertices[0] - vertices[2])
             if vA.dot(vB) < 0:
                 vertices[2], vertices[3] = vertices[3], vertices[2]
+            elif vB.dot(vC) < 0:
+                vertices[2], vertices[1] = vertices[1], vertices[2]
 
         self.vertices.extend(vertices)
         self.faces.append(vert_count)
