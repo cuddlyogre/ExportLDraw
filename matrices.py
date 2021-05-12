d9b5d50dd9df7e481bce8dabc49ffa500e16fe96
Matthew Morrison <cuddlyogre@gmail.com>
2021-05-12 11:46:23 -0500

    math functions

--- matrices.py
+++ matrices.py
@@ -62,3 +62,34 @@ def mt4(matrix):
         matrix[2],
         matrix[3],
     ))
+
+
+# https://stackoverflow.com/a/48266808
+def unit_vector(vector):
+    """ Returns the unit vector of the vector."""
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """Finds angle between two vectors"""
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def x_rotation(vector, theta):
+    """Rotates 3-D vector around x-axis"""
+    R = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+    return np.dot(R, vector)
+
+
+def y_rotation(vector, theta):
+    """Rotates 3-D vector around y-axis"""
+    R = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]])
+    return np.dot(R, vector)
+
+
+def z_rotation(vector, theta):
+    """Rotates 3-D vector around z-axis"""
+    R = np.array([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    return np.dot(R, vector)
