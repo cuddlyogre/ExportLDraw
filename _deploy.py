61c7e3a15bfa55509e459c1ee3555bd45606922e
Matthew Morrison <cuddlyogre@gmail.com>
2021-05-27 22:36:47 -0500

    dev

---
+++ _deploy.py
@@ -0,0 +1,21 @@
+import os
+from shutil import copytree, ignore_patterns, rmtree
+
+app_data_dir = os.path.expandvars(r'%APPDATA%\Blender Foundation\Blender')
+version = '2.92'
+addons_dir = r'scripts\addons'
+blender_dir = os.path.join(app_data_dir, version, addons_dir)
+if not os.path.isdir(blender_dir):
+    exit(f"{blender_dir} does not exist")
+
+addon_name = 'ExportLdraw'
+target = os.path.join(blender_dir, addon_name)
+print(f"deploying to {target}")
+
+this_script_dir = os.path.dirname(os.path.realpath(__file__))
+patterns = {".git", "__pycache__", ".idea"}
+copytree(this_script_dir, target, dirs_exist_ok=True, ignore=ignore_patterns(*patterns))
+
+pycache_dir = os.path.join(target, "__pycache__")
+if os.path.isdir(pycache_dir):
+    rmtree(pycache_dir)
