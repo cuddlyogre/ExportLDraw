5c7b51fb9c77fa5dc185c5cf6386afd7e251f6e7
Matthew Morrison <cuddlyogre@gmail.com>
2021-08-26 04:32:41 -0500

    part type

--- ldraw_part_types.py
+++ ldraw_part_types.py
@@ -1,7 +1,7 @@
 model_types = ["model", "unofficial_model", "un-official model", "submodel", None]
 part_types = ["part", "unofficial_part", "un-official part"]
 shortcut_types = ["shortcut", "unofficial_shortcut", "un-official shortcut"]
-subpart_types = ["primitive", "subpart", "un-official primitive", "un-official subpart"]
+subpart_types = ["primitive", "subpart", "un-official primitive", "un-official subpart", "unofficial_subpart"]
 plain_stud_names = ["stud.dat", "stud2.dat"]
 stud_names = [
     "stud.dat", "stud-logo.dat", "stud-logo2.dat", "stud-logo3.dat", "stud-logo4.dat", "stud-logo5.dat",
