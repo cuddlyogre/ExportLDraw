83d9f5bbd2eacec7fcbddfe359031ad046ab1935
Matthew Morrison <cuddlyogre@gmail.com>
2021-10-07 12:37:55 -0500

    deploy

--- _deploy.py
+++ _deploy.py
@@ -2,22 +2,25 @@ import os
 import sys
 from shutil import copytree, ignore_patterns, rmtree
 
+version = sys.argv[1]
+addon_name = sys.argv[2]
+
 app_data_dir = os.path.expandvars(r'%APPDATA%\Blender Foundation\Blender')
-version = '2.93'
-if len(sys.argv) > 1:
-    version = sys.argv[1]
 addons_dir = r'scripts\addons'
 blender_dir = os.path.join(app_data_dir, version, addons_dir)
+target = os.path.join(blender_dir, addon_name)
+
 if not os.path.isdir(blender_dir):
     exit(f"{blender_dir} does not exist")
 
-addon_name = 'ExportLDraw'
-target = os.path.join(blender_dir, addon_name)
 print(f"deploying to {target}")
 
 this_script_dir = os.path.dirname(os.path.realpath(__file__))
-patterns = {".git", "__pycache__", ".idea"}
-rmtree(target)
+patterns = {".git", "__pycache__", ".idea", ".test"}
+
+if os.path.isdir(target):
+    rmtree(target)
+
 copytree(this_script_dir, target, dirs_exist_ok=True, ignore=ignore_patterns(*patterns))
 
 pycache_dir = os.path.join(target, "__pycache__")
