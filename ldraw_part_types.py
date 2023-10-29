025d2c11871a726ebc10d23d5d689c7ba7d591ef
Matthew Morrison <cuddlyogre@gmail.com>
2023-10-28 21:19:08 -0500

    logos

--- ldraw_part_types.py
+++ ldraw_part_types.py
@@ -5,9 +5,12 @@ shortcut_types = ["shortcut", "unofficial_shortcut", "un-official shortcut"]
 subpart_types = ["subpart", "un-official subpart", "unofficial_subpart"]
 primitive_types = ["primitive", "un-official primitive"]
 plain_stud_names = ["stud.dat", "stud2.dat"]
+
 stud_names = [
     "stud.dat", "stud-logo.dat", "stud-logo2.dat", "stud-logo3.dat", "stud-logo4.dat", "stud-logo5.dat",
     "stud2.dat", "stud2-logo.dat", "stud2-logo2.dat", "stud2-logo3.dat", "stud2-logo4.dat", "stud2-logo5.dat",
 ]
+
 edge_logo_names = ["logo.dat", "logo2.dat"]
 logo_names = ["logo3.dat", "logo4.dat", "logo5.dat"]
+logos = ["logo", "logo2", "logo3", "logo4", "logo5", "high-contrast"]
